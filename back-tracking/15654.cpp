05f111d09bbc40aa0b096559ba82bd706a8f170f
박진용 <jinyoung1997@gmail.com>
2021-02-18 15:16:50 +0900

    solve 15654

---
+++ back-tracking/15654.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int n, m;
+vector<int> v, e;
+bool check[9];
+
+void dfs(int M){
+  if(M == m){
+    for(int x:e){
+      printf("%d ", x);
+    }
+    puts("");
+    return;
+  }
+  for(int i=0; i<v.size(); i++){
+    if(!check[i]){
+      e.push_back(v[i]);
+      check[i] = true;
+      dfs(M + 1);
+      e.pop_back();
+      check[i] = false;
+    }
+  }
+}
+
+int main ()
+{
+  scanf("%d%d", &n, &m);
+  while(n--){
+    int x;
+    scanf("%d", &x);
+    v.push_back(x);
+  }
+
+  sort(v.begin(), v.end());
+  dfs(0);
+
+}
